55d9e412938a3b601af12c7455c19d2ce8b5875a
Aaron Lal <lucidlear@gmail.com>
2024-02-04 15:03:56 +1100

    8 rendering data to templates

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,32 @@ from . models import *
 # Create your views here.
 
 def home(request):
-  return render(request, 'accounts/dashboard.html')
+  orders = Order.objects.all()
+  customers = Customer.objects.all()
+
+  total_customers = customers.count()
+
+  total_orders = orders.count()
+
+  delivered = orders.filter(status='delivered').count()
+  print(delivered)
+  pending = orders.filter(status='Pending').count()
+
+  context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+  'total_orders': total_orders, 'delivered': delivered, 'pending': pending }
+
+  return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
   products = Product.objects.all()
+
   return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-  return render(request, 'accounts/customer.html')
+def customer(request, pk):
+  customer = Customer.objects.get(id=pk)
+
+  orders = customer.order_set.all()
+  order_count = orders.count()
+
+  context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+  return render(request, 'accounts/customer.html', context)
