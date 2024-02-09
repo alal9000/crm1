6fdfa3436ab477bdeea8aa6ad4c5cd0ee415cf67
Aaron Lal <lucidlear@gmail.com>
2024-02-09 20:35:48 +1100

    user profile with one to one relationship

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,7 @@ def registerPage(request):
 
       group = Group.objects.get(name='customer')
       user.groups.add(group)
-      # Customer.objects.create(user=user, name=user.username)
+      Customer.objects.create(user=user, name=user.username)
 
       messages.success(request, "account was created for " + username)
       return redirect('login')
@@ -65,7 +65,6 @@ def home(request):
   total_orders = orders.count()
 
   delivered = orders.filter(status='delivered').count()
-  print(delivered)
   pending = orders.filter(status='Pending').count()
 
   context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
@@ -77,14 +76,13 @@ def home(request):
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['customer'])
 def userPage(request):
-  # orders = request.user.customer.order_set.all()
+  orders = request.user.customer.order_set.all()
 
-  # total_orders = orders.count()
-  # delivered = orders.filter(status='delivered').count()
-  # pending = orders.filter(status='Pending').count()
-  # print('Orders', orders)
-  # context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending }
-  return render(request, 'accounts/user.html')
+  total_orders = orders.count()
+  delivered = orders.filter(status='delivered').count()
+  pending = orders.filter(status='Pending').count()
+  context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending }
+  return render(request, 'accounts/user.html', context)
 
 
 @login_required(login_url='login')
